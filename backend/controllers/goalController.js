69a0b14ad4db26b18ba1596ebda41e5e53cb9bb3
Iurii Lopatenko <yalopatenko@gmail.com>
2022-11-26 13:18:18 -0800

    Part 1 is finished
    Goal schema and controller

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,12 +1,12 @@
 const asynkHandler = require('express-async-handler');
-
+const Goal = require('../models/goalModel');
 // @desc    Get all the Goals
 // @route   GET to /api/goals
 // @access  Private
 const getGoals = asynkHandler(async (req, res) => {
-  res.status(200).json({ message: 'Get all the Goals!' });
+  const goals = await Goal.find();
+  res.status(200).json(goals);
 });
-
 // @desc    Create a new Goal
 // @route   POST to /api/goals
 // @access  Private
@@ -15,25 +15,38 @@ const setGoal = asynkHandler(async (req, res) => {
     res.status(400);
     throw new Error('Please add Text field!!!');
   }
-  res.status(201).json(req.body);
+  const goal = await Goal.create({ text: req.body.text });
+  res.status(201).json(goal);
 });
-
 // @desc    Update a Goal by ID
 // @route   PUT to /api/goals/:id
 // @access  Private
 const updateGoal = asynkHandler(async (req, res) => {
-  res
-    .status(200)
-    .json({ message: `Update a Goal with ID = '${req.params.id}'` });
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error('Please add Text field!!!');
+  }
+  const goal = await Goal.findById(req.params.id);
+  if (!goal) {
+    res.status(400);
+    throw new Error('Goal not found!');
+  }
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.status(200).json(updatedGoal);
 });
-
 // @desc    Delete a Goal by ID
 // @route   DELETE to /api/goals/:id
 // @access  Private
 const deleteGoal = asynkHandler(async (req, res) => {
-  res
-    .status(200)
-    .json({ message: `Delete a Goal with ID = '${req.params.id}'` });
+  const goal = await Goal.findById(req.params.id);
+  if (!goal) {
+    res.status(400);
+    throw new Error('Goal not found!');
+  }
+  await goal.remove();
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = { getGoals, setGoal, updateGoal, deleteGoal };
