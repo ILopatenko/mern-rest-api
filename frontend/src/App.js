4001cd96bff09173f7b25ef18405c5c545eb0d5d
Iurii Lopatenko <yalopatenko@gmail.com>
2022-11-29 21:04:42 -0800

    Part 3. TC:17:00
    Main empty pages and Header were created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Header from './components/Header';
+
 const App = () => {
   return (
-    <div>
-      <h1>My app</h1>
-    </div>
+    <Router>
+      <>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Dashboard />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/register" element={<Register />}></Route>
+          </Routes>
+        </div>
+      </>
+    </Router>
   );
 };
 
